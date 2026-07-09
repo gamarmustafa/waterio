e1adc22211a7e9f5f944db81ef0b24350e9d590e
Gamar Mustafa <gamar.mustafa@idda.az>
2026-07-09 10:28:48 +0400

    Initial Commit

---
+++ waterio/waterioApp.swift
@@ -0,0 +1,17 @@
+//
+//  waterioApp.swift
+//  waterio
+//
+//  Created by Gamar Mustafa on 09.07.26.
+//
+
+import SwiftUI
+
+@main
+struct waterioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
